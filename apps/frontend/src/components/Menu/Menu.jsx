f36ac743aed0badded78a66eb4de32752fb2624b
jochong27 <jochong27@gmail.com>
2025-07-12 12:22:44 +0200

    add new configs

---
+++ apps/frontend/src/components/Menu/Menu.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import MenuStyles from "./Menu.module.scss";
+
+const Menu = () => {
+  return (
+    <nav>
+      <ul className={MenuStyles.menu_container}>
+        <li className={MenuStyles.menu_container_item}>
+          <a href="/">Accueil</a>
+        </li>
+        <li className={MenuStyles.menu_container_item}>
+          <a href="/">Joueurs</a>
+        </li>
+        <li className={MenuStyles.menu_container_item}>
+          <a href="/">Teams</a>
+        </li>
+        <li className={MenuStyles.menu_container_item}>
+          <a href="/login">Connexion</a>
+        </li>
+        <li className={MenuStyles.menu_container_item}>
+          <a href="/signup">Inscription</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Menu;
