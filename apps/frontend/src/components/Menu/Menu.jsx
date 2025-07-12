0059c58af3caf9aedc351351951e0a343dc9836e
jong <jochong27@gmail.com>
2025-07-12 23:27:19 +0200

    finish login page

--- apps/frontend/src/components/Menu/Menu.jsx
+++ apps/frontend/src/components/Menu/Menu.jsx
@@ -19,7 +19,7 @@ const Menu = () => {
           <a href="/auth/login">Connexion</a>
         </li>
         <li className={MenuStyles.menu_container_item}>
-          <a href="/signup">Inscription</a>
+          <a href="/auth/signup">Inscription</a>
         </li>
       </ul>
     </nav>
