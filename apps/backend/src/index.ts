0ffff890b7dfd1f326a5433a707a7a332caac86f
jong <jochong27@gmail.com>
2025-09-05 08:44:30 +0200

    fix: update server port to 4000 and add Dockerfile for deployment

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -24,7 +24,7 @@ app.use("/api/announces", announceRoutes);
 app.use("/api/conversations", conversationRoutes);
 app.use("/api/messages", messageRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`➡️  Server running at http://localhost:${PORT}`);
 });
